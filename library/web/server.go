d31827a6665a90c04ab27999b202a479c128de8f
Laisky.Cai <github@laisky.com>
2023-03-03 16:04:36 +0000

    fix: logger

--- library/web/server.go
+++ library/web/server.go
@@ -3,27 +3,43 @@ package web
 import (
 	"net/http"
 
-	"github.com/Laisky/go-ramjet/library/log"
-
 	gmw "github.com/Laisky/gin-middlewares/v5"
-	gconfig "github.com/Laisky/go-config/v2"
+	// gconfig "github.com/Laisky/go-config/v2"
+	"github.com/Laisky/go-ramjet/library/log"
+	glog "github.com/Laisky/go-utils/v4/log"
 	"github.com/Laisky/zap"
 	"github.com/gin-gonic/gin"
 )
 
 var (
-	Server = gin.New()
+	Server *gin.Engine
 )
 
-func RunServer(addr string) {
-	if !gconfig.Shared.GetBool("debug") {
-		gin.SetMode(gin.ReleaseMode)
-	}
-
+func init() {
+	Server = gin.New()
 	Server.Use(
 		gin.Recovery(),
-		gmw.NewLoggerMiddleware(gmw.WithLogger(log.Logger)),
+		gmw.NewLoggerMiddleware(
+			gmw.WithLoggerMwColored(),
+			gmw.WithLevel(glog.LevelInfo),
+			gmw.WithLogger(log.Logger),
+		),
 	)
+}
+
+func RunServer(addr string) {
+	// if !gconfig.Shared.GetBool("debug") {
+	// 	gin.SetMode(gin.ReleaseMode)
+	// }
+
+	// Server.Use(
+	// 	gin.Recovery(),
+	// 	gmw.NewLoggerMiddleware(
+	// 		gmw.WithLoggerMwColored(),
+	// 		gmw.WithLevel(glog.LevelInfo),
+	// 		gmw.WithLogger(log.Logger),
+	// 	),
+	// )
 
 	if err := gmw.EnableMetric(Server); err != nil {
 		log.Logger.Panic("enable metrics", zap.Error(err))
