ee690c53f279f503ec5a80b772915ed4bd014a3a
Laisky.Cai <github@laisky.com>
2023-06-15 13:32:27 +0000

    feat: part of sso

---
+++ internal/tasks/sso/server.go
@@ -0,0 +1,29 @@
+// Package gptchat implements gptchat tasks.
+package gptchat
+
+import (
+	"net/http"
+
+	"github.com/Laisky/zap"
+	"github.com/gin-gonic/gin"
+
+	ihttp "github.com/Laisky/go-ramjet/internal/tasks/gptchat/http"
+	istatic "github.com/Laisky/go-ramjet/internal/tasks/gptchat/templates/static"
+	"github.com/Laisky/go-ramjet/library/log"
+	"github.com/Laisky/go-ramjet/library/web"
+)
+
+func bindHTTP() {
+	// FIXME not implemented
+	if err := ihttp.SetupHTTPCli(); err != nil {
+		log.Logger.Panic("setup http client", zap.Error(err))
+	}
+
+	ihttp.RegisterStatic(web.Server.Group("/static"))
+	web.Server.GET("/favicon.ico", func(ctx *gin.Context) {
+		ctx.Header("Cache-Control", "max-age=86400")
+		ctx.Data(http.StatusOK, "image/png", istatic.Favicon)
+	})
+	web.Server.Any("/api/", ihttp.APIHandler)
+	web.Server.GET("/", ihttp.Chat)
+}
