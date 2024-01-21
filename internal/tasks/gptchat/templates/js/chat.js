cb27e1c0c4d12b528ceef4a1cc3cca9b50052dc4
Laisky.Cai <github@laisky.com>
2024-01-21 02:33:25 +0000

    style: Adjust CSS styles and disable HTTPS crawler
    - Reduce padding and fix radius button in the chat component.
    - Disable the HTTPS crawler in the chat switch component.

--- internal/tasks/gptchat/templates/js/chat.js
+++ internal/tasks/gptchat/templates/js/chat.js
@@ -1886,7 +1886,7 @@ function newSessionConfig () {
         system_prompt: "The following is a conversation with Chat-GPT, an AI created by OpenAI. The AI is helpful, creative, clever, and very friendly, it's mainly focused on solving coding problems, so it likely provide code example whenever it can and every code block is rendered as markdown. However, it also has a sense of humor and can talk about anything. Please answer user's last question, and if possible, reference the context as much as you can.",
         selected_model: ChatModelTurbo35V1106,
         chat_switch: {
-            disable_https_crawler: false,
+            disable_https_crawler: true,
             enable_google_search: false
         }
     }
